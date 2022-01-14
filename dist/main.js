77bac401dd8e6c848cfea596b15d8a6c0e200fab
Alexandr <alex10_08@mail.ru>
2022-01-14 20:54:11 +0100

    показывать второй телефон

--- dist/main.js
+++ dist/main.js
@@ -1,1 +1,96 @@
-(()=>{"use strict";console.log("tel")})();
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./index.js":
+/*!******************!*\
+  !*** ./index.js ***!
+  \******************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _modules_tel__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./modules/tel */ \"./modules/tel.js\");\n\n(0,_modules_tel__WEBPACK_IMPORTED_MODULE_0__[\"default\"])()\n\n//# sourceURL=webpack:///./index.js?");
+
+/***/ }),
+
+/***/ "./modules/tel.js":
+/*!************************!*\
+  !*** ./modules/tel.js ***!
+  \************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\nconst tel = () => {\n    const arrow = document.querySelector('.header-contacts__arrow')\n    const divPhone = document.querySelector('.header-contacts__phone-number-accord')\n    const phone = document.querySelector('.header-contacts__phone-number-accord>a')\n    let change = true\n\n    arrow.addEventListener('click', (e) => {\n        console.dir(e.target);\n        if (change) {\n            divPhone.style.position = 'relative'\n            phone.style.webkitTransition = '0.5s'\n            phone.style.transition = '0.5s'\n            phone.style.opacity = '1'\n            change = false\n        }\n        else {\n            divPhone.style.position = 'absolute'\n            phone.style.webkitTransition = '0s'\n            phone.style.transition = '0s'\n            phone.style.opacity = '0'\n            change = true\n        }\n    })\n}\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (tel);\n\n//# sourceURL=webpack:///./modules/tel.js?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/define property getters */
+/******/ 	(() => {
+/******/ 		// define getter functions for harmony exports
+/******/ 		__webpack_require__.d = (exports, definition) => {
+/******/ 			for(var key in definition) {
+/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
+/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
+/******/ 				}
+/******/ 			}
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
+/******/ 	(() => {
+/******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	(() => {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = (exports) => {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	var __webpack_exports__ = __webpack_require__("./index.js");
+/******/ 	
+/******/ })()
+;
--- src/modules/tel.js
+++ src/modules/tel.js
@@ -1,4 +1,25 @@
 const tel = () => {
-    console.log('tel');
+    const arrow = document.querySelector('.header-contacts__arrow')
+    const divPhone = document.querySelector('.header-contacts__phone-number-accord')
+    const phone = document.querySelector('.header-contacts__phone-number-accord>a')
+    let change = true
+
+    arrow.addEventListener('click', (e) => {
+        console.dir(e.target);
+        if (change) {
+            divPhone.style.position = 'relative'
+            phone.style.webkitTransition = '0.5s'
+            phone.style.transition = '0.5s'
+            phone.style.opacity = '1'
+            change = false
+        }
+        else {
+            divPhone.style.position = 'absolute'
+            phone.style.webkitTransition = '0s'
+            phone.style.transition = '0s'
+            phone.style.opacity = '0'
+            change = true
+        }
+    })
 }
 export default tel
