8f90bcc15d5a2e8781c771315ab5da4065766229
Alexandr <alex10_08@mail.ru>
2022-01-15 00:05:42 +0100

    плавная прокрутка

--- src/modules/menu.js
+++ src/modules/menu.js
@@ -1,10 +1,12 @@
 const menu = () => {
     const menu = document.querySelector('.popup-dialog-menu')
+    const menuItem = document.querySelectorAll('.popup-menu-nav__item>a')
+
 
 
 
     document.addEventListener('click', (e) => {
-        console.log(e.target);
+
         if (e.target.closest('.menu__icon')) {
             e.preventDefault()
             menu.style.transform = 'translate3d(0px, 0, 0)'
@@ -12,6 +14,22 @@ const menu = () => {
         }
         else if (e.target.closest('.close-menu') || !e.target.closest('.row')) {
             menu.removeAttribute('style')
+        }
+        menuItem.forEach(item => {
+            if (e.target === item) {
+                e.preventDefault()
+                let linkhref = e.target.attributes.href.value.slice(1)
+                let link = document.getElementById(linkhref)
+                link.scrollIntoView({ behavior: "smooth" })
+                menu.removeAttribute('style')
+            }
+        })
+        if (e.target.closest('.button-footer')) {
+            e.preventDefault()
+            window.scrollTo({
+                top: 0,
+                behavior: 'smooth'
+            });
         }
     })
 }
