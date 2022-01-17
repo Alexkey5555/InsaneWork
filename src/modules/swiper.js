482a2e9c1b78c5b62a61b91a5709d259ad80e58f
Alexandr <alex10_08@mail.ru>
2022-01-17 23:39:35 +0100

    адаптивный слайдер

--- src/modules/swiper.js
+++ src/modules/swiper.js
@@ -4,7 +4,7 @@ import Swiper, { Navigation, Pagination } from 'swiper';
 Swiper.use([Navigation, Pagination]);
 
 const swiper = () => {
-
+    const width = window.screen.width
     const swiper = new Swiper('.formula-slider-wrap', {
         slidesPerView: 3,
         spaceBetween: 10,
@@ -16,6 +16,18 @@ const swiper = () => {
     });
     const formulaHide = document.querySelector('.formula-slider-wrap')
     formulaHide.style.overflow = 'hidden';
+
+    if (width <= 1024) {
+        const swiper2 = new Swiper('.repair-types-nav', {
+            slidesPerView: 3,
+            spaceBetween: 10,
+
+            navigation: {
+                nextEl: ".nav-arrow_right",
+                prevEl: ".nav-arrow_left",
+            },
+        })
+    }
 
 }
 export default swiper
